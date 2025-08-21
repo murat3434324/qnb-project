1ca5d42d210a7c0df616935bb96f15ea8450efcb
Murat Demir <atakisiyevseymur14@gmail.com>
2025-08-21 13:28:26 +0300

    +

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
   description: "çekiliş fırsatları",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
